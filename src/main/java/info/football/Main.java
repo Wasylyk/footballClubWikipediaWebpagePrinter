af6542656e40000007cf252e4f3f462fc75e2855
Tomasz Wasyłyk <tomasz.wasylyk@gmail.com>
2020-10-01 12:36:28 +0200

    remove fixed string literal from main method, and use command line argument instead

--- src/main/java/info/football/Main.java
+++ src/main/java/info/football/Main.java
@@ -5,7 +5,8 @@ import info.football.service.FootBallClubInfoService;
 public class Main {
 
     public static void main(String[] args) {
+
         FootBallClubInfoService footBallClubInfoService = new FootBallClubInfoService();
-        System.out.println(footBallClubInfoService.findLinkToClub("liverpool"));
+        System.out.println(footBallClubInfoService.findLinkToClub(args[0]));
     }
 }
